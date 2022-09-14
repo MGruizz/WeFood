5d3fee12818847dfc2a811d66621dd9986d8e703
Sebastian Moyano <90289493+SebaIMR@users.noreply.github.com>
2022-09-14 18:48:54 -0300

    Creacion Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+<<<<<<< Updated upstream
 import { InicioComponent } from './inicio/inicio.component';
 import { PerfilComponent } from './perfil/perfil.component';
+=======
+import { LoginComponent } from './login/login.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     InicioComponent,
     PerfilComponent
+=======
+    LoginComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
