404c88556529249120a8e53a278096f18ca92d8a
MGruizz <74569397+MGruizz@users.noreply.github.com>
2022-09-14 20:30:14 -0300

    Actualizacion directorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { InicioComponent } from './pages/inicio/inicio.component'
+import { PerfilComponent } from './pages/perfil/perfil.component';
+const routes: Routes = [
+  {path:'', component: InicioComponent },
+  {path:'perfil', component: PerfilComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
