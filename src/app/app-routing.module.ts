2c13b6c7de81202215ed85f0cc10317a8bd2bbce
Sebastian Moyano <90289493+SebaIMR@users.noreply.github.com>
2022-09-14 21:11:48 -0300

    Añadi CreacionUsuarioComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { LoginComponent } from './pages/login/login.component';
 import { InfoRecetaComponent } from './pages/info-receta/info-receta.component';
+import { CreacionEdicionRecetaComponent } from './pages/creacion-edicion-receta/creacion-edicion-receta.component';
+import { CreacionUsuarioComponent } from './pages/creacion-usuario/creacion-usuario.component';
+
 const routes: Routes = [
   {path:'', component: InicioComponent },
   {path:'perfil', component: PerfilComponent },
   {path:'login', component: LoginComponent},
   {path:'info-receta', component: InfoRecetaComponent},
+  {path:'creacion-edicion-receta', component: CreacionEdicionRecetaComponent},
+  {path:'creacion-usuario', component: CreacionUsuarioComponent},
 ];
 
 @NgModule({
