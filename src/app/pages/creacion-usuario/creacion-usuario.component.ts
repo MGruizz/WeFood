beedae4770f4a32c1dd7c5e24497e903d2819d88
beiache <makbeiache@gmail.com>
2022-09-24 18:15:47 -0300

    Formulario creacion-usuario

--- src/app/pages/creacion-usuario/creacion-usuario.component.ts
+++ src/app/pages/creacion-usuario/creacion-usuario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-creacion-usuario',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creacion-usuario.component.scss']
 })
 export class CreacionUsuarioComponent implements OnInit {
+  formularioCreacionUsuarioForm : FormGroup = {} as FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    let form ={
+      username: ['', Validators.compose([
+        Validators.pattern(/^.{1,20}$/),
+        Validators.required
+      ])],
+      email: ['', Validators.compose([
+        Validators.pattern(/^.{5,}$/),
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.pattern(/^.{5,}$/),
+        Validators.required
+      ])],
+      confirmPassword: ['', Validators.compose([
+        Validators.pattern(/^.{5,}$/),
+        Validators.required
+      ])],
+    }
+
+    this.formularioCreacionUsuarioForm = this.formBuilder.group(form);
   }
 
+  registrarse(){
+    console.log(this.formularioCreacionUsuarioForm.status);
+    if (this.formularioCreacionUsuarioForm.status === 'VALID'){
+      this.router.navigate(['/inicio'])
+    }
+  }
 }
