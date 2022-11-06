785be92df4bcf2fbef6e45808d4c5a5587afa064
Sebastian Moyano <90289493+SebaIMR@users.noreply.github.com>
2022-11-06 19:43:10 -0300

    esta bastian

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import {HttpClientModule} from "@angular/common/http";
         AppRoutingModule,
         NgbModule,
         ReactiveFormsModule,
-        HttpClientModule
+        HttpClientModule,
+        BrowserAnimationsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
